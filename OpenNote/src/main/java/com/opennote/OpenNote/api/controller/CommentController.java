87f2c188022e0f60329aacaf5635c10fabb718e9
Adya <aparida@syr.edu>
2024-02-22 16:36:27 -0500

    Adding comment service, comment controller and comment Junit test files

--- OpenNote/src/main/java/com/opennote/OpenNote/api/controller/CommentController.java
+++ OpenNote/src/main/java/com/opennote/OpenNote/api/controller/CommentController.java
@@ -1,4 +1,30 @@
 package com.opennote.OpenNote.api.controller;
+// The CommentController exposes REST endpoints for posting, editing, and deleting comments,
+// and it delegates the actual operations to the CommentService.
 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/comments")
 public class CommentController {
+
+    @Autowired
+    private CommentService commentService;
+
+    @PostMapping
+    public Comment postComment(@RequestBody Comment comment) {
+        return commentService.postComment(comment);
+    }
+
+    @PutMapping("/{id}")
+    public Comment editComment(@PathVariable("id") int commentID, @RequestBody Comment comment) {
+        return commentService.editComment(commentID, comment);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteComment(@PathVariable("id") int commentID) {
+        commentService.deleteComment(commentID);
+    }
 }
