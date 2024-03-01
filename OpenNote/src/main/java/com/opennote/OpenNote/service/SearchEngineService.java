f732dc0d83d5422a98d0b202ef691038a64b9647
Raasin Amin Rahman <77879314+RaasinRahman@users.noreply.github.com>
2024-03-01 14:51:33 -0500

    Update SearchEngineService.java

--- OpenNote/src/main/java/com/opennote/OpenNote/service/SearchEngineService.java
+++ OpenNote/src/main/java/com/opennote/OpenNote/service/SearchEngineService.java
@@ -1,4 +1,42 @@
 package com.opennote.OpenNote.service;
 
+import com.opennote.OpenNote.api.model.Note;
+import com.opennote.OpenNote.repository.NoteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class SearchEngineService {
+
+    private final NoteRepository noteRepository;
+
+    @Autowired
+    public SearchEngineService(NoteRepository noteRepository) {
+        this.noteRepository = noteRepository;
+    }
+
+    public List<Note> searchNotes(String keywords) {
+  
+        return noteRepository.findByKeywords(keywords);
+    }
+
+   
+    public List<Note> filterByCategory(String category) {
+        
+        return noteRepository.findByCategory(category);
+    }
+
+   
+    public List<Note> sortByDate() {
+        
+        return noteRepository.findAllByOrderByDateDesc();
+    }
+
+  
+    public List<Note> sortByPopularity() {
+        
+        return noteRepository.findAllByOrderByPopularityDesc();
+    }
 }
