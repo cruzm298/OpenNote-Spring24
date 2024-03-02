8a0e29b39e64771292bcede269eaeae7a29346ef
Adya Parida <82367239+adyaparida@users.noreply.github.com>
2024-03-01 22:29:22 -0500

    Added code for AdminController.java

--- OpenNote/src/main/java/com/opennote/OpenNote/api/controller/AdminController.java
+++ OpenNote/src/main/java/com/opennote/OpenNote/api/controller/AdminController.java
@@ -1,4 +1,32 @@
 package com.opennote.OpenNote.api.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/admins")
 public class AdminController {
+
+    @Autowired
+    private AdminService adminService;
+
+    @DeleteMapping("/users/{id}")
+    public void deleteUser(@PathVariable("id") int userID) {
+        adminService.deleteUser(userID);
+    }
+
+    @DeleteMapping("/notes/{id}")
+    public void deleteNote(@PathVariable("id") int noteID) {
+        adminService.deleteNote(noteID);
+    }
+
+    @PutMapping("/categories")
+    public void manageCategories() {
+        adminService.manageCategories();
+    }
+
+    @GetMapping("/statistics")
+    public void viewStatistics() {
+        adminService.viewStatistics();
+    }
 }
